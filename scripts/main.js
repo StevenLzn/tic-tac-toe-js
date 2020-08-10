f8543188caa5bb566b9b27f7253e0e5838239a5f
StevenLzn <stevenlozano17@gmail.com>
2020-08-10 11:30:39 -0500

    Terminada funcionalidad, se agrego funcionalidad para determinar el ganador, se agregaron aspectos visuales iniciales, se agregaran mejoras visuales para una mejor experiencia

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,97 @@
 let turnX = true;
-const winningConditions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+let cellsX = [];
+let cellsO = [];
+
+const winningConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
 const cells = document.querySelectorAll('.cell');
+const playerX = document.querySelector('.player-x');
+const playerY = document.querySelector('.player-y');
+const startButton = document.querySelectorAll('button');
 
-for (let i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('click', clickCell);
-    cells[i].param = i;
+startButton[0].addEventListener('click', startGame);
+
+startGame();
+
+function startGame() {
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].innerHTML = '';
+        cells[i].addEventListener('click', clickCell);
+        cells[i].param = i;
+    }
+    turnX = true;
+    cellsX = [];
+    cellsO = [];
+
+    if (turnX) {
+        playerX.classList.remove('winner');
+        playerY.classList.remove('actual-turn');
+        playerX.classList.add('actual-turn');
+    } else {
+        playerY.classList.remove('winner');
+        playerX.classList.remove('actual-turn');
+        playerY.classList.add('actual-turn');
+    }
 }
 
-
 function clickCell(e) {
+    let winningCount = 0;
+    let compareCells;
+
     for (let i = 0; i < cells.length; i++) {
         if (i == e.target.param) {
             if (turnX) {
                 cells[i].innerHTML = 'X'
-            } else{
-                cells[i].innerHTML = 'O' 
-            }   
+                cells[i].removeEventListener('click', clickCell);
+                cellsX.push(i);
+                compareCells = cellsX;
+            } else {
+                cells[i].innerHTML = 'O'
+                cells[i].removeEventListener('click', clickCell);
+                cellsO.push(i);
+                compareCells = cellsO;
+            }
         }
     }
+
+    for (let i = 0; i < winningConditions.length; i++) {
+        for (let j = 0; j < compareCells.length; j++) {
+            if (winningConditions[i].includes(compareCells[j])) {
+                winningCount += 1;
+            }
+        }
+        if (winningCount == 3) {
+            winGame();
+            return;
+        } else {
+            winningCount = 0;
+        }
+
+    }
+
     turnX = !turnX;
+
+    if (turnX) {
+        playerY.classList.remove('actual-turn');
+        playerX.classList.add('actual-turn');
+    } else {
+        playerX.classList.remove('actual-turn');
+        playerY.classList.add('actual-turn');
+    }
+}
+
+function winGame() {
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].removeEventListener('click', clickCell);
+        cells[i].param = i;
+    }
+
+    if (turnX) {
+        playerX.classList.add('winner');
+        playerX.classList.remove('actual-turn');
+    } else {
+        playerY.classList.add('winner');
+        playerY.classList.remove('actual-turn');
+    }
+
 }
